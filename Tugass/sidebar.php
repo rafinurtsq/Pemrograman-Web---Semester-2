b49e5a0b509faee53797fa65ac46de1c3565f716
Rafi Nur Tsaqif Akhmad <141921478+rafinurtsq@users.noreply.github.com>
2026-05-13 19:42:40 +0700

    Delete Tugass directory

--- Tugass/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="list-group">
-  <a href="index.php?hal=home" class="list-group-item list-group-item-action active" aria-current="true">
-    Menu Utama
-  </a>
-  <a href="index.php?hal=home" class="list-group-item list-group-item-action">Halaman Home</a>
-  <a href="index.php?hal=about" class="list-group-item list-group-item-action">About Me</a>
-  <a href="index.php?hal=contact" class="list-group-item list-group-item-action">Contact Me</a>
-  <a href="index.php?hal=studies" class="list-group-item list-group-item-action">My Studies</a>
-</div>
